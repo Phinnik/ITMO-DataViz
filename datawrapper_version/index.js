a0092d4eb7b2daf700bfaef76eabd2451e37a87c
Philipp <phinnik@mail.ru>
2020-08-16 20:01:05 +0300

    add interactive reading state bar

--- datawrapper_version/index.js
+++ datawrapper_version/index.js
@@ -1,12 +1,42 @@
+$(document).ready(function () {
 
-// navigation construction
-const headers = $('.paragraph-header');
-headers.each(function (i) {
-    let nav_item = $("<li></li>")
-        .addClass('navigation-item');
-    let nav_a = $("<a></a>")
-        .text(( this ).textContent);
-    nav_item.append(nav_a);
-    $("#navigation-menu").append(nav_item);
-    nav_item.click(() => this.scrollIntoView())
-});
+    // navigation construction
+    const paragraphPositions = [];
+
+    $('.paragraph-container').each(function () {
+        paragraphPositions.push(($(this).offset().top));
+        let headerText = $(this).children('.paragraph-header')[0].textContent;
+
+        let nav_item = $("<li></li>")
+            .addClass('navigation-item');
+        let nav_a = $("<a></a>")
+            .text(headerText);
+        nav_item.append(nav_a);
+        $("#navigation-menu").append(nav_item);
+        nav_item.click(() => this.scrollIntoView())
+    });
+
+
+    let currentParagraphIndex = 0;
+    $(`.navigation-item:eq(${currentParagraphIndex})`).toggleClass('navigation-item-current');
+
+
+    $(document).scroll(function () {
+        const currentScreenY = document.documentElement.scrollTop;
+
+        for (let i = 0; i < paragraphPositions.length; i++) {
+            if (currentParagraphIndex !== i && paragraphPositions[i] <= currentScreenY && currentScreenY <= paragraphPositions[i + 1] - 20) {
+                $(`.navigation-item:eq(${currentParagraphIndex})`).toggleClass('navigation-item-current');
+                $(`.navigation-item:eq(${i})`).toggleClass('navigation-item-current');
+                currentParagraphIndex = i;
+
+            }
+        }
+
+    })
+
+
+})
+
+
+
